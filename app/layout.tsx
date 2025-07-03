a947a95af89fcdc14181a862066df2fda7fda03b
OGtwelve <ogtwelve@ogtwelve.com.cn>
2025-07-03 15:43:53 +0800

    add SpeedInsights for more access information

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="zh-CN">
       <body className={inter.className}>{children}</body>
+      <SpeedInsights />
     </html>
   )
 }
